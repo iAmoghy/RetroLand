9eebfadc80ddef5147af682149f8027375b6263e
iAmoghy <70843557+iAmoghy@users.noreply.github.com>
2022-08-11 08:18:25 +0100

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -43,73 +43,73 @@ var submitAnswer = function() {
             }
         } else if (genreVal == "adventure") {
             if (platVal == "nintendo") {
-                window.location.href = "https://www.retrogames.cz/play_001-NES.php";
+                window.location.href = "https://www.retrogames.cz/play_1570-NES.php";
             } else {
-                window.location.href = "https://www.retrogames.cz/play_001-NES.php";
+                window.location.href = "https://www.retrogames.cz/play_352-Atari2600.php";
             }
         } else if (genreVal == "platformer") {
             if (platVal == "nintendo") {
-                window.location.href = "https://www.retrogames.cz/play_001-NES.php";
+                window.location.href = "https://www.retrogames.cz/play_003-NES.php";
             } else {
-                window.location.href = "https://www.retrogames.cz/play_001-NES.php";
+                window.location.href = "https://www.retrogames.cz/play_029-Atari2600.php";
             }
         } else if (genreVal == "sport") {
             if (platVal == "nintendo") {
-                window.location.href = "https://www.retrogames.cz/play_001-NES.php";
+                window.location.href = "https://www.retrogames.cz/play_057-NES.php";
             } else {
-                window.location.href = "https://www.retrogames.cz/play_001-NES.php";
+                window.location.href = "https://www.retrogames.cz/play_664-Atari2600.php";
             }
         }
     } else if (eraVal == "1990s") {
         if (genreVal == "action") {
             if (platVal == "nintendo") {
-                window.location.href = "https://www.retrogames.cz/play_001-NES.php";
+                window.location.href = "https://www.retrogames.cz/play_1061-N64.php";
             } else {
-                window.location.href = "https://www.retrogames.cz/play_001-NES.php";
+                window.location.href = "https://www.retrogames.cz/play_973-DOS.php";
             }
         } else if (genreVal == "adventure") {
             if (platVal == "nintendo") {
-                window.location.href = "https://www.retrogames.cz/play_1102-SNES.php";
+                window.location.href = "https://www.retrogames.cz/play_900-SNES.php";
             } else {
-                window.location.href = "https://www.retrogames.cz/play_1102-SNES.php";
+                window.location.href = "https://www.retrogames.cz/play_1360-DOS.php";
             }
         } else if (genreVal == "platformer") {
             if (platVal == "nintendo") {
-                window.location.href = "https://www.retrogames.cz/play_001-NES.php";
+                window.location.href = "https://www.retrogames.cz/play_1066-N64.php";
             } else {
-                window.location.href = "https://www.retrogames.cz/play_001-NES.php";
+                window.location.href = "https://www.retrogames.cz/play_479-DOS.php";
             }
         } else if (genreVal == "sport") {
             if (platVal == "nintendo") {
-                window.location.href = "https://www.retrogames.cz/play_001-NES.php";
+                window.location.href = "retrogames.cz/play_1617-Genesis.php";
             } else {
-                window.location.href = "https://www.retrogames.cz/play_001-NES.php";
+                window.location.href = "https://www.retrogames.cz/play_1311-Genesis.php";
             }
         }
     }  else if (eraVal == "2000s") {
         if (genreVal == "action") {
             if (platVal == "nintendo") {
-                window.location.href = "https://www.retrogames.cz/play_001-NES.php";
+                window.location.href = "https://www.retrogames.cz/play_1443-NES.php";
             } else {
-                window.location.href = "https://www.retrogames.cz/play_001-NES.php";
+                window.location.href = "https://www.retrogames.cz/play_737-DOS.php";
             }
         } else if (genreVal == "adventure") {
             if (platVal == "nintendo") {
-                window.location.href = "https://www.retrogames.cz/play_001-NES.php";
+                window.location.href = "https://www.retrogames.cz/play_1608-N64.php";
             } else {
                 window.location.href = "https://www.retrogames.cz/play_001-NES.php";
             }
         } else if (genreVal == "platformer") {
             if (platVal == "nintendo") {
-                window.location.href = "https://www.retrogames.cz/play_001-NES.php";
+                window.location.href = "https://www.retrogames.cz/play_1108-N64.php";
             } else {
-                window.location.href = "https://www.retrogames.cz/play_001-NES.php";
+                window.location.href = "https://www.retrogames.cz/play_736-DOS.php";
             }
         } else if (genreVal == "sport") {
             if (platVal == "nintendo") {
-                window.location.href = "https://www.retrogames.cz/play_001-NES.php";
+                window.location.href = "https://www.retrogames.cz/play_1337-NES.php";
             } else {
-                window.location.href = "https://www.retrogames.cz/play_001-NES.php";
+                window.location.href = "https://www.retrogames.cz/play_1461-Atari7800.php";
             }
         }
     }
--- js/script.js
+++ js/script.js
@@ -23,3 +23,15 @@ document.getElementById("action").addEventListener("click", function() {
 document.getElementById("platformer").addEventListener("click", function() {
   window.location.href = ""
 });
+
+
+if (screen.width < 1280 || screen.height < 800) {
+  console.log("too small")
+  alert("This site is not intended to be run on mobile devices! Please use a PC/laptop for the best experience.")
+}
+
+function copyLink() {
+  console.log("RUNNING")
+  navigator.clipboard.writeText("https://iamoghy.github.io/RetroLand/")
+  alert("Text copied! Share it wherever you would like :)")
+}
